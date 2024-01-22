50f7890af3a0c8ee28a9594130b734d7820340e3
Y. Xu <xymailmooc@163.com>
2024-01-22 11:39:54 +0800

    fix: set example author list to any

--- src/modules/preferenceScript.ts
+++ src/modules/preferenceScript.ts
@@ -104,7 +104,7 @@ function bindPrefEvents() {
     addon.data.prefs!.window.document.querySelectorAll(
       `#zotero-prefpane-${config.addonRef} checkbox,input,menulist`,
     );
-  const exampleAuthorList: Zotero.Item.Creator[] = [
+  const exampleAuthorList: any = [
     {
       creatorTypeID: 8,
       fieldMode: 0,
