fe0f5673b1f205db59ab22158867ba8378714607
Northword <northword@outlook.com>
2025-02-28 22:05:11 +0800

    fix: shutdown failed

--- src/addon.ts
+++ src/addon.ts
@@ -1,9 +1,11 @@
 import hooks from "./hooks";
+import { config } from "../package.json";
 import { createZToolkit } from "./utils/ztoolkit";
 
 class Addon {
   public data: {
     alive: boolean;
+    config: typeof config;
     // Env type, see build.js
     env: "development" | "production";
     ztoolkit: ZToolkit;
@@ -22,6 +24,7 @@ class Addon {
   constructor() {
     this.data = {
       alive: true,
+      config,
       env: __env__,
       ztoolkit: createZToolkit(),
     };
